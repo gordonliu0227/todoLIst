9778ba6feb79af7d29ee21aa6f16dcc86f9c0dbe
Gordon <gordonliu0227@users.noreply.github.com>
2021-03-30 13:04:52 +1000

    a practice for component tree
    input text area deliver to an independent component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,25 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
+  
 
-  function addItem() {
-    setItems((prevItems) => {
+  function addItem(inputText) {
+    setItems(prevItems => {
       return [...prevItems, inputText];
     });
-    setInputText("");
+   
   }
 
   function deleteItem(id) {
-    setItems(prevItems =>{
-      return prevItems.filter((items, index)=>{
-        return index !==id
-      })
-    })
-    
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
   }
 
   return (
@@ -31,12 +27,7 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onAdd = {addItem}/>
       <div>
         <ul>
           {items.map((todoItem, index) => (
--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props) {
   return (
-    <div onClick={()=>{
-      props.onChecked(props.id)
-    }}>
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
       <li>{props.text}</li>
     </div>
   );
